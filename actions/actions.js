4ca482cb547c09b919a4321a01c66725ff5021ea
bendiumpope <ama.benedict@yahoo.com>
2022-05-19 15:38:15 +0100

    create the done and remove command

--- actions/actions.js
+++ actions/actions.js
@@ -44,3 +44,57 @@ export const add = async (todo) => {
     console.log(error);
   }
 };
+
+export const done = async (todoId) => {
+  try {
+    const todosList = await fs.readFile(
+      `${process.cwd()}/data/store.json`,
+      'utf8'
+    );
+
+    const todos = todosList ? JSON.parse(todosList) : [];
+    
+    if (todos.length <= 0 || (+todoId) > todos.length) {
+      console.log('You do not have a todo with this Id or your todo list is empty');
+    }else{
+
+      const newTodos = todos.map((todo, index)=>{
+
+        if((index + 1) === (+todoId)){
+          todo.done ? (todo.done = false) : (todo.done = true);
+        }
+
+        return todo;
+        
+      });
+
+      await fs.writeFile(`./data/store.json`, JSON.stringify(newTodos));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const remove = async (todoId) => {
+  try {
+    const todosList = await fs.readFile(
+      `${process.cwd()}/data/store.json`,
+      'utf8'
+    );
+
+    const todos = todosList ? JSON.parse(todosList) : [];
+
+    if (todos.length <= 0 || (+todoId) > todos.length) {
+      console.log(
+        'You do not have a todo with this Id or your todo list is empty'
+      );
+    } else {
+      const newTodos = todos.filter((todo, index) => index + 1 !== (+todoId));
+
+      console.log(newTodos);
+      await fs.writeFile(`./data/store.json`, JSON.stringify(newTodos));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 import { program } from 'commander';
-import { list, add } from './actions/actions.js';
+import { list, add, done, remove } from './actions/actions.js';
 
 program
   .command('list')
@@ -12,5 +12,15 @@ program
 .command('add <todo>')
 .description('Add a todo')
 .action(add)
+
+program
+  .command('done <todoId>')
+  .description('take the todo number and mark the todo as done')
+  .action(done);
+
+program
+  .command('delete <todoId>')
+  .description('take the todo number and delete the todo')
+  .action(remove);
 
 program.parse();
